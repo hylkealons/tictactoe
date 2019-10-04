0b9e0defa1a3bb3abff0c521cc76dae76f3192c9
Hylke Alons <halons@digitalorigin.com>
2019-10-04 17:32:05 +0200

    Add mix test.all command for CI checking

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule TicTacToe.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      preferred_cli_env: ["test.all": :test, dialyzer: :dev],
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -20,6 +22,25 @@ defmodule TicTacToe.MixProject do
       {:sobelow, "~> 0.7", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.1.2", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      "test.all": [
+        # Check formatting
+        "format --dry-run --check-formatted",
+        # Check no compiler errors
+        "compile --warnings-as-errors",
+        # Check tests succeed
+        "test",
+        # Check proper code
+        "credo",
+        # Check no security errors
+        "sobelow",
+        # Check types
+        "dialyzer"
+      ]
     ]
   end
 end
