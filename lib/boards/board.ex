fef85ca6d68ef8ac548ea588ca63f0c3a310c53c
Hylke Alons <halons@digitalorigin.com>
2019-10-04 17:39:36 +0200

    Board context and the ability to create a board and set a column

---
+++ lib/boards/board.ex
@@ -0,0 +1,27 @@
+defmodule TicTacToe.Boards.Board do
+  @moduledoc """
+  The `Board` represents the playing board.
+  """
+
+  alias TicTacToe.Players.Player
+
+  @type column :: Player.t() | nil
+  @type row :: pos_integer
+  @type t :: {column, column, column, column, column, column, column, column, column}
+
+  @doc """
+  Returns a new board. The board is represented is flattened and returned as a tuple.
+  """
+  @spec new() :: t
+  def new(), do: Tuple.duplicate(nil, 9)
+
+  @spec set(t, Player.t(), row, column) :: {:ok, t} | {:error, term}
+  def set(board, player, row, column) when row in [1, 2, 3] and column in [1, 2, 3] do
+    case elem(board, row * column) do
+      nil -> {:ok, put_elem(board, row * column, player)}
+      _ -> {:error, :already_set}
+    end
+  end
+
+  def set(_, _, _, _), do: {:error, :out_of_bounds}
+end
---
+++ test/boards/board_test.exs
@@ -0,0 +1,36 @@
+defmodule TicTacToe.Boards.BoardTest do
+  use ExUnit.Case
+
+  alias TicTacToe.Boards.Board
+
+  @empty_board {nil, nil, nil, nil, nil, nil, nil, nil, nil}
+
+  describe "new/0" do
+    test "creates a new board" do
+      assert Board.new() == @empty_board
+    end
+  end
+
+  describe "set/4" do
+    test "sets the values on the board" do
+      {:ok, board} = Board.set(@empty_board, :x, 2, 2)
+      {:ok, board} = Board.set(board, :x, 3, 2)
+      assert elem(board, 4) == :x
+      assert elem(board, 6) == :x
+    end
+
+    test "cannot set the value twice" do
+      {:ok, board} = Board.set(@empty_board, :x, 2, 2)
+      assert Board.set(board, :x, 2, 2) == {:error, :already_set}
+    end
+
+    test "cannot set out of bounds" do
+      valid_value = 2
+
+      Enum.each([0, -1, 4], fn invalid_value ->
+        assert Board.set(@empty_board, :x, invalid_value, valid_value) == {:error, :out_of_bounds}
+        assert Board.set(@empty_board, :x, valid_value, invalid_value) == {:error, :out_of_bounds}
+      end)
+    end
+  end
+end
