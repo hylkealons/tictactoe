1ba87840f69662f6eec1c096611ae357136da0d5
Hylke Alons <halons@digitalorigin.com>
2019-10-08 20:20:26 +0200

    Implement the make move action

--- lib/actions/make_move.ex
+++ lib/actions/make_move.ex
@@ -5,6 +5,13 @@ defmodule TicTacToe.Actions.MakeMove do
 
   @behaviour TicTacToe.Actions.Concerns.Action
 
+  alias TicTacToe.Games.Game
+  alias TicTacToe.Boards.Rule
+  alias TicTacToe.Boards.Board
+  alias TicTacToe.Players.Player
+
+  @adapter Application.get_env(:tic_tac_toe, Game)[:adapter] || raise("Game adapter must be set")
+
   @type params :: %{
           id: any,
           player: :x | :o,
@@ -16,7 +23,7 @@ defmodule TicTacToe.Actions.MakeMove do
   @spec call(params) :: {:error, term}
   def call(params) do
     case validate_input(params) do
-      :ok -> {:error, :not_implemented}
+      :ok -> move(params.id, params.player, params.row, params.column)
       {:error, reason} -> {:error, reason}
     end
   end
@@ -29,6 +36,26 @@ defmodule TicTacToe.Actions.MakeMove do
       params[:row] not in 1..3 -> {:error, :invalid_row}
       is_nil(params[:id]) -> {:error, :id_not_set}
       true -> :ok
+    end
+  end
+
+  @spec move(Game.id(), Player.t(), Board.row(), Board.column()) :: {:ok, Game.t()}
+  defp move(game_id, player, row, column) do
+    with {:ok, game} <- @adapter.get_game(game_id),
+         {:is_turn_player, true} <- {:is_turn_player, player == game.current_player},
+         {:ok, board} <- Board.set(game.board, player, row, column),
+         :no_winner <- Rule.board_winner(board) do
+      updated_game =
+        game
+        |> Map.put(:board, board)
+        |> Map.put(:current_player, Player.next(game.current_player))
+
+      @adapter.update_game(updated_game)
+
+      {:ok, updated_game}
+    else
+      {:is_turn_player, false} -> {:error, :turn_other_player}
+      error -> error
     end
   end
 end
--- test/actions/make_move_test.exs
+++ test/actions/make_move_test.exs
@@ -1,17 +1,39 @@
 defmodule TicTacToe.Actions.MakeMoveTest do
   use ExUnit.Case
 
+  alias TicTacToe.Games.Game
   alias TicTacToe.Actions.MakeMove
+  alias TicTacToe.Games.Game.State.Ets
 
   describe "call/1" do
     setup do
-      valid_params = %{player: :x, column: 3, row: 1, id: "id"}
+      Ets.init()
+      {:ok, game} = Ets.start_game(:o)
+      valid_params = %{player: :o, column: 3, row: 1, id: game.id}
 
-      %{valid_params: valid_params}
+      %{valid_params: valid_params, game: game}
     end
 
     test "returns the expected response when called", context do
-      assert MakeMove.call(context.valid_params) == {:error, :not_implemented}
+      expected_game = %Game{
+        board: {nil, nil, :o, nil, nil, nil, nil, nil, nil},
+        current_player: :x,
+        id: context.game.id
+      }
+
+      assert MakeMove.call(context.valid_params) == {:ok, expected_game}
+    end
+
+    test "when a game is won", context do
+      current_board = {:o, :o, nil, nil, nil, nil, nil, nil, nil}
+      context.game |> Map.put(:board, current_board) |> Ets.update_game()
+      params = %{player: :o, column: 3, row: 1, id: context.game.id}
+      assert MakeMove.call(params) == {:winner, {:o, :horizontal, :row1}}
+    end
+
+    test "when it's not the players turn", context do
+      params = Map.put(context.valid_params, :player, :x)
+      assert MakeMove.call(params) == {:error, :turn_other_player}
     end
 
     test "when a invalid player is given", context do
